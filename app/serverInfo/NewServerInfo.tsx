3a073042bc7d9dac906fb0ff4a862b44e3979e44
Jacky Fan <contact@jacky.fan>
2023-05-19 18:36:13 +0800

    UI: add-server page

--- app/serverInfo/NewServerInfo.tsx
+++ app/serverInfo/NewServerInfo.tsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, Icon, Input, KeyboardAvoidingView, NativeBaseProvider, ScrollView, Text, VStack, View } from "native-base";
+import { Box, Button, FormControl, Icon, Input, KeyboardAvoidingView, NativeBaseProvider, ScrollView, Text, VStack, View } from "native-base";
 import AppBar from "../../components/AppBar";
 import PageTitle from "../../components/PageTitle";
 import { MaterialIcons, Ionicons, Entypo, AntDesign } from '@expo/vector-icons';
@@ -18,43 +18,55 @@ export default function NewServerInfo() {
                     <KeyboardAvoidingView>
                         <PageTitle icon="playlist-add" title="Add Server" />
                         <VStack w="full" space={2}>
-                            <FormControl isInvalid={false} w="75%" mx="auto">
-                                <FormControl.Label>Server IP</FormControl.Label>
-                                <Input placeholder="192.168.1.1 or mc.example.com" />
-                                <FormControl.ErrorMessage leftIcon={<Icon as={MaterialIcons} name="error" size="xs" />}>
-                                    Try different from previous passwords.
-                                </FormControl.ErrorMessage>
-                            </FormControl>
-                            <FormControl isInvalid={false} w="75%" mx="auto">
-                                <FormControl.Label>Port</FormControl.Label>
-                                <Input placeholder="25565" defaultValue="25565" keyboardType="numeric" />
-                                <FormControl.ErrorMessage leftIcon={<Icon as={MaterialIcons} name="error" size="xs" />}>
-                                    Try different from previous passwords.
-                                </FormControl.ErrorMessage>
-                            </FormControl>
-                            <FormControl isInvalid={false} w="75%" mx="auto">
-                                <FormControl.Label>Username</FormControl.Label>
-                                <Input placeholder="Enter username" />
-                                <FormControl.ErrorMessage leftIcon={<Icon as={MaterialIcons} name="error" size="xs" />}>
-                                    Try different from previous passwords.
-                                </FormControl.ErrorMessage>
-                            </FormControl>
-                            <FormControl isInvalid={false} w="75%" mx="auto">
-                                <FormControl.Label>Password</FormControl.Label>
-                                <Input placeholder="Enter password" type="password" />
-                                <FormControl.ErrorMessage leftIcon={<Icon as={MaterialIcons} name="error" size="xs" />}>
-                                    Try different from previous passwords.
-                                </FormControl.ErrorMessage>
-                            </FormControl>
+                            <Box bg="white" mx="4" my="1" p="4" rounded="xl">
+                                <FormControl isInvalid={false} w="full" mx="auto">
+                                    <FormControl.Label>Server IP</FormControl.Label>
+                                    <Input placeholder="192.168.1.1 or mc.example.com" />
+                                    <FormControl.ErrorMessage leftIcon={<Icon as={MaterialIcons} name="error" size="xs" />}>
+                                        Try different from previous passwords.
+                                    </FormControl.ErrorMessage>
+                                </FormControl>
+                            </Box>
+                            <Box bg="white" mx="4" my="1" p="4" rounded="xl">
+                                <FormControl isInvalid={false} w="full" mx="auto">
+                                    <FormControl.Label>Port</FormControl.Label>
+                                    <Input placeholder="25565" defaultValue="25565" keyboardType="numeric" />
+                                    <FormControl.ErrorMessage leftIcon={<Icon as={MaterialIcons} name="error" size="xs" />}>
+                                        Try different from previous passwords.
+                                    </FormControl.ErrorMessage>
+                                </FormControl>
+                            </Box>
+                            <Box bg="white" mx="4" my="1" p="4" rounded="xl">
+                                <FormControl isInvalid={false} w="full" mx="auto">
+                                    <FormControl.Label>Username</FormControl.Label>
+                                    <Input placeholder="Enter username" />
+                                    <FormControl.ErrorMessage leftIcon={<Icon as={MaterialIcons} name="error" size="xs" />}>
+                                        Try different from previous passwords.
+                                    </FormControl.ErrorMessage>
+                                </FormControl>
+                            </Box>
+
+                            <Box bg="white" mx="4" my="1" p="4" rounded="xl">
+                                <FormControl isInvalid={false} w="full" mx="auto">
+                                    <FormControl.Label>Password</FormControl.Label>
+                                    <Input placeholder="Enter password" type="password" />
+                                    <FormControl.ErrorMessage leftIcon={<Icon as={MaterialIcons} name="error" size="xs" />}>
+                                        Try different from previous passwords.
+                                    </FormControl.ErrorMessage>
+                                </FormControl>
+                            </Box>
+                            <Box  mx="4" my="1" rounded="xl">
                             <Button
-                                mt={2}
-                                w="75%"
+                                w="full"
                                 mx="auto"
+                                rounded="xl"
                                 onPress={() => console.log("hello world")}
                             >
                                 Add Server
                             </Button>
+                            </Box>
                         </VStack>
+
                     </KeyboardAvoidingView>
                 </ScrollView>
             </View>
